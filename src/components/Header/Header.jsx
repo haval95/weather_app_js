4dfcab4dc64981130729339fef98ca2004ef175b
haval95 <40795444+halenkurdi@users.noreply.github.com>
2021-02-07 22:31:57 +0300

    header Component structure created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import './Header.css';
 const Header = (props) => {
     return (
-        <div>
-            <div></div>
+        <div className="HeaderItems">
+            <div> Rain: {props.rain}</div>
+            <div> Humidity: {props.humidity}</div>
+            <div>Wind: {props.wind}</div>
+            <div>Pressure: {props.pressure}</div>
+            <div> Dew Point: {props.dewPoint}</div>
+            <div>UV Index: {props.UVindex}</div>
+            <div>Visibility: {props.visibility}</div>
             
         </div>
     )
--- src/container/WeatherApp.jsx
+++ src/container/WeatherApp.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Header from '../components/Header/Header'
+import React1, { useState } from 'react';
 
 export default function WeatherApp() {
+    const [HeaderData, setHeaderData]= useState( {"rain":"none", "humidity": 20, "wind": 26, "pressure":85, "dewPoint": 25.5, "UVindex":66, "visibility":35} )
+
+   
     return (
         <div>
-            <Header />
+            <Header     rain={HeaderData.rain} 
+                        humidity={HeaderData.humidity} 
+                        wind={HeaderData.wind} 
+                        pressure={HeaderData.pressure} 
+                        dewPoint={HeaderData.dewPoint}
+                        UVindex={HeaderData.UVindex}
+                        visibility={HeaderData.visibility}
+                        />
         </div>
     )
 }
